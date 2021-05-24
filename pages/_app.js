b5cc506521c7c8a4b4a8c0ae985de43c8313e4e7
JarrodKane <kajarrod@gmail.com>
2021-05-24 10:36:27 +1000

    - Added in Favicon icons - Added in favicon into the head of _app.js to display on all pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Head from "next/head";
 import { ThemeProvider } from "styled-components";
 import { lightTheme, darkTheme, GlobalStyles } from "../styles/ThemeConfig";
 import "../styles/globals.css";
@@ -11,11 +12,39 @@ function MyApp({ Component, pageProps }) {
   };
 
   return (
-    <ThemeProvider theme={theme == "light" ? lightTheme : darkTheme}>
-      <GlobalStyles />
-      {/* <button onClick={toggleTheme}>Switch Theme</button> */}
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <>
+      <Head>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="128x128"
+          href="/images/favicon/favicon-128x128.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/images/favicon/favicon-16x16.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/images/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="196x196"
+          href="/images/favicon/favicon-196x196.png"
+        />
+      </Head>
+      <ThemeProvider theme={theme == "light" ? lightTheme : darkTheme}>
+        <GlobalStyles />
+        {/* <button onClick={toggleTheme}>Switch Theme</button> */}
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
   );
 }
 export default MyApp;
