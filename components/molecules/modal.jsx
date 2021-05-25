a8a99076a196aedbf17208ecc4667abee079145a
JarrodKane <kajarrod@gmail.com>
2021-05-25 11:04:54 +1000

    - Fix to the css classes so there is a space applied

--- components/molecules/modal.jsx
+++ components/molecules/modal.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const Modal = ({ isOpen = false, children }) => {
   return (
     <div
-      className={`modal bg-red-500 h-screen opacity-95 fixed w-full left-0 overflow-hidden origin-top top-14  duration-300  z-30 transform scale-y-0${
-        isOpen ? "scale-y-100" : ""
+      className={`modal bg-red-500 h-screen opacity-95 fixed w-full left-0 overflow-hidden origin-top top-14  duration-300  z-30  scale-y-0 transform ${
+        isOpen ? "scale-y-100 " : ""
       }`}
     >
       {/* Needed a negative margin to account for the navigation bar at the top */}
